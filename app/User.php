1f1d2c2298bb5b39ae9f4690cd46780f2d7606ee
reszi <gabor.reszler413@gmail.com>
2020-03-22 00:49:17 +0100

    make reminder attributes of user fillable

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'reminded_daily', 'reminded_weekly', 'reminded_monthly',
     ];
 
     /**
