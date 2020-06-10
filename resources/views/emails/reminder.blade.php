aaa95f1a681ce8f4a280822974a54ca49ecdddfb
reszi <gabor.reszler413@gmail.com>
2020-06-10 22:02:50 +0200

    create reminder email blade using markdown

---
+++ resources/views/emails/reminder.blade.php
@@ -0,0 +1,14 @@
+@component('mail::message')
+# Dear {{ $user->name }},
+here's your {{ $interval }} reminder about your TV shows' episodes aired {{ $interval_text }} and you haven't seen them (yet):
+
+@foreach($episodes as $tv_show_title => $value)
+- {{ $tv_show_title }}
+@foreach ($value as $episode)
+	- S{{ str_pad($episode->season->season_number, 2, "0", STR_PAD_LEFT) }}E{{ str_pad($episode->episode_number, 2, "0", STR_PAD_LEFT) }} {{ $episode->title }}
+@endforeach
+@endforeach
+
+Enjoy your TV shows,<br>
+{{ config('app.name') }}
+@endcomponent
